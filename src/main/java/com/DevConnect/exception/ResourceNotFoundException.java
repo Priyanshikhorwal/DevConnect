d501944c7442cad84e42a24abe5f8a11c7b6f861
PriyanshiKhorwal <priyanshikhorwal0905@gmail.com>
2026-03-12 19:36:26 +0530

    Resource not found exception file setting

--- src/main/java/com/DevConnect/exception/ResourceNotFoundException.java
+++ src/main/java/com/DevConnect/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.DevConnect.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
