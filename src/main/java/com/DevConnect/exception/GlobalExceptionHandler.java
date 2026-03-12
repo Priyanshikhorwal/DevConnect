a06c264ed4c5b3cd522eb5bd261149cd57b1aeef
PriyanshiKhorwal <priyanshikhorwal0905@gmail.com>
2026-03-12 19:38:20 +0530

    Global exception handler file setting

--- src/main/java/com/DevConnect/exception/GlobalExceptionHandler.java
+++ src/main/java/com/DevConnect/exception/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package com.DevConnect.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException ex){
+
+        ApiError error = new ApiError(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGeneralException(Exception ex){
+
+        ApiError error = new ApiError(
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
